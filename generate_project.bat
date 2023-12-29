213abe949350b2bd93f94794d739399aed91aa01
Andrei Stroe <andrei.stroe2000@yahoo.com>
2023-12-29 02:40:13 +0200

    Added premake as a build system to the project

---
+++ generate_project.bat
@@ -0,0 +1,2 @@
+call 3rdparties\premake\premake5.exe vs2022
+pause
---
+++ premake5.lua
@@ -0,0 +1,105 @@
+workspace "Corylium"
+	architecture "x86_64"
+	
+	configurations 
+	{ 
+		"Debug",
+		"Release",
+		"Dist"
+	}
+	
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Corylium"
+	location "Corylium"
+	kind "SharedLib"
+    language "C++"
+	
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp" 
+	}
+	
+	includedirs
+	{
+		"%{prj.name}/3rdparties/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"CRLY_PLATFORM_WINDOWS",
+			"CRLY_BUILD_DLL"
+		}
+		
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} %{wks.location}bin/" .. outputdir .. "/Sandbox")
+		}
+		
+    filter "configurations:Debug"
+		defines "CRLY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CRLY_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CRLY_DIST"
+		optimize "On"
+		
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+	
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp" 
+	}
+	
+	includedirs
+	{
+		"Corylium/3rdparties/spdlog/include",
+		"Corylium/src"
+	}
+	
+	links
+	{
+		"Corylium"
+	}
+
+	filter "system:windows"
+		cppdialect "C++20"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"CRLY_PLATFORM_WINDOWS",
+		}
+		
+    filter "configurations:Debug"
+		defines "CRLY_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "CRLY_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "CRLY_DIST"
+		optimize "On"
