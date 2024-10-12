fe5d46b2c656f6ace6cc74bb29f8a89b9e5d66f0
Andrei Stroe <andrei.stroe2000@yahoo.com>
2024-10-12 20:39:34 +0300

    Add WindowCloseEvent

--- Corylium/src/Corylium/Event/ApplicationEvent.h
+++ Corylium/src/Corylium/Event/ApplicationEvent.h
@@ -4,21 +4,34 @@
 
 namespace Corylium {
 
-	class CORYLIUM_API WindowResizeEvent : public Event
+	class WindowResizeEvent : public Event
 	{
 	public:
-		WindowResizeEvent(unsigned int width, unsigned int height)
-			: _width(width), _height(height)
+		WindowResizeEvent(unsigned int Width, unsigned int Height)
+			: _width(Width), _height(Height)
 		{}
-
-		unsigned int GetWidth() const { return _width; }
-		unsigned int GetHeight() const { return _height; }
 
 		[[nodiscard]] virtual EventType GetEventType() const override { return EventType::WindowResize; }
 		[[nodiscard]] virtual std::string_view GetName() const override { return "WindowResizeEvent"; }
 		[[nodiscard]] virtual int GetCategoryFlags() const override { return EventCategory::EventCategoryApplication; }
+
+		unsigned int GetWidth() const { return _width; }
+		unsigned int GetHeight() const { return _height; }
+	
 	private:
 		unsigned int _width;
 		unsigned int _height;
+	};
+
+	class WindowCloseEvent : public Event
+	{
+	public:
+		WindowCloseEvent()
+		{}
+
+		[[nodiscard]] virtual EventType GetEventType() const override { return EventType::WindowClose; }
+		[[nodiscard]] virtual std::string_view GetName() const override { return "WindowCloseEvent"; }
+		[[nodiscard]] virtual int GetCategoryFlags() const override { return EventCategory::EventCategoryApplication; }
+	
 	};
 }
